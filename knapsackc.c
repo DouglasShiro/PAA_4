81a5f09e944c894df3cb3a6c64228352fdabd783
douglasshiro <douglasshiro@hotmail.com>
2016-06-11 16:29:59 -0300

    knapsack

--- knapsackc.c
+++ knapsackc.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// #include <cstdio>
-// #include <cstdlib>
-//#include <algorithm> max
-// #include <iostream>
-// #include <numeric>
-//#include <vector> vector
+//http://www.spoj.com/problems/KNAPSACK/
+
 
 int maior(int a, int b){
-    return (a<b)?b:a;
+    return a<b ? b:a;
 }
 
 int knapsack(int v[], size_t sizeV, int w[], int W){
     int n = sizeV;
     int F[W+1];
-    for(int i = 0; i < n; ++i){
-        for(int a = W; a >= w[i]; --a){
-            F[a] = maior(F[a], F[a - w[i] + v[i]]);
+
+    for(int i = 0; i < W+1; i++)
+        F[i] = 0;
+
+    for(int i = 0; i < n; ++i)
+        for(int a = W; a >= w[i]; --a)
+            F[a] = maior(F[a], F[a - w[i]] + v[i]);
+
     return F[W];
 }
 
@@ -27,7 +28,7 @@ int main(){
     int v[n];
     int w[n];
 
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++)
         scanf("%d %d", &w[i], &v[i]);
 
     printf("%d\n", knapsack(v, sizeof v / sizeof *v, w, W));

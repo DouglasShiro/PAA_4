7ffc5e077d7888957e535a33d13a59d00fa079c7
douglasshiro <douglasshiro@hotmail.com>
2016-06-11 10:45:53 -0300

    algoritmos

--- edist.c
+++ edist.c
@@ -1,4 +1,4 @@
-/* 
+/*
 *                          UNIVERSIDADE DE BRASILIA
 *                                   1/2016
 *                       Projeto e Analise de Algoritmos
@@ -6,7 +6,57 @@
 *                      Douglas Shiro Yokoyama 13/0024902
 *
 *******************************************************************************
-*	http://www.spoj.com/problems/EDIST/ 
+* 	http://www.spoj.com/problems/EDIST/
 *
 */
+#include<stdio.h>
+#include<string.h>
 
+int dist[2002][2002];
+
+int diff(char i , char j)
+{
+	if(i == j) return 0;
+	else return 1;
+}
+
+int min(int i , int j, int k)
+{
+	if(i <= j)
+	{
+		if(i <= k) return i;
+		return k;
+	}
+	else
+	{
+		if(j <= k) return j;
+		return k;
+	}
+}
+
+int main()
+{
+	int len1,len2,i,j,t;
+	char str1[2002];
+	char str2[2002];
+	scanf("%d",&t);
+	while(t--)
+	{
+		scanf("%s",str1);
+		scanf("%s",str2);
+		len1 = strlen(str1);
+		len2 = strlen(str2);
+		for(i = 0 ; i <= len1 ; i++)
+			for(j = 0 ; j <= len2 ; j++)
+				dist[i][j] = 0;
+		for(i = 0 ; i <= len1 ; i++)
+			dist[i][0] = i;
+		for(i = 0 ; i <= len2 ; i++)
+			dist[0][i] = i;
+		for(i = 1 ; i <= len1 ; i++)
+			for(j = 1 ; j <= len2 ; j++)
+				dist[i][j] = min(1+dist[i-1][j],1+dist[i][j-1],diff(str1[i-1],str2[j-1])+dist[i-1][j-1]);
+		printf("%d\n",dist[len1][len2]);
+	}
+	return 0;
+}
---
+++ knapsackc.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// #include <cstdio>
+// #include <cstdlib>
+//#include <algorithm> max
+// #include <iostream>
+// #include <numeric>
+//#include <vector> vector
+
+int maior(int a, int b){
+    return (a<b)?b:a;
+}
+
+int knapsack(int v[], size_t sizeV, int w[], int W){
+    int n = sizeV;
+    int F[W+1];
+    for(int i = 0; i < n; ++i){
+        for(int a = W; a >= w[i]; --a){
+            F[a] = maior(F[a], F[a - w[i] + v[i]]);
+    return F[W];
+}
+
+int main(){
+    int W, n;
+    scanf("%d %d", &W, &n);
+    int v[n];
+    int w[n];
+
+    for(int i = 0; i < n; i++){
+        scanf("%d %d", &w[i], &v[i]);
+
+    printf("%d\n", knapsack(v, sizeof v / sizeof *v, w, W));
+
+return 0;
+}
